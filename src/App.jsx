1ebda330447cb0c94e57e754bddcb14b52d6f4c9
Florian <f.schaessens@gmail.com>
2024-07-12 22:33:05 +0200

    setup Card

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CreditCard from './components/CreditCard'
+
+function App() {
+  return (
+    <>
+      <CreditCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,31 @@
+import "./CreditCard.css"
+
+
+export default function CreditCard () {
+
+    return (
+        <div id="cardContainer">
+            
+        </div>
+    )
+}
+
+
+
+
+//Texte Recto
+
+// POUL’ EPARGNE
+// 1843 9236 0046 8346
+// CARTE DE CREDIT DEBIT
+// EXPIRE A FIN
+// 07/28
+// MME GALINA C. NUGGETS
+
+
+//Texte Verso
+
+// Pondu le 12/07/2024 à 21h04
+// SIGNATURE DU TITULAIRE OBLIGATOIRE
+// PE LIBRE REPONSE 50299 - 93755 PLUMEBOURG CEDEX.
+// Important : carte strictement personnelle, propriété de la Poul’ Épargne émettrice qui peut la retirer à tout moment. Tout poulet trouvant cette carte est prié de bien vouloir l’adresser sous pli non affranchi à l’adresse ci dessus.
